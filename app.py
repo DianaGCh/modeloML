cc53ebe93e5cda3f82f87293d7d75c8151ccf454
Gabriela <dggonzalesch@unl.edu.ec>
2022-02-21 01:26:30 -0500

    configuracion

--- app.py
+++ app.py
@@ -6,40 +6,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-#@app.route('/')
-#def home():
-  #  return 'MODELO DE PREDICCION CON MACHINE LEARNING'
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    item = [x for x in request.form.values()]
-    data = []
+    return 'MODELO DE PREDICCION CON MACHINE LEARNING'
 
-    data.append(int(item[0]))
-    if item[1] == 'Male':
-        data.append(0)
-        data.append(1)
-    else:
-        data.append(1)
-        data.append(0)
-
-    if item[2] == 'No':
-        data.append(1)
-        data.append(0)
-    else:
-        data.append(0)
-        data.append(1)
-    
-    prediction = model.predict([data])
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='The Insurance cost will be   $ {}'.format(output))
 
 if __name__ == "__main__":
     app.run()
