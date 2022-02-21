67b195e00e620c88e87b2b32463bd56e61e48c6e
Gabriela <dggonzalesch@unl.edu.ec>
2022-02-20 20:58:44 -0500

    configuracion

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from flask import Flask
-from pyspark import F
+#from pyspark import F
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,7 @@ CORS(app)
 def home():
     return 'MODELO DE PREDICCION CON MACHINE LEARNING'
 
-@app.route("/predecir", methods=["POST"])
-def predecir():
-    return 'Predicciones page'
+
 
 if __name__ == "__main__":
     app.run()
