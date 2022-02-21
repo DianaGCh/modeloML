06b1de9ec8fc8324f6a5b81742dd1e7c9378d3b6
Gabriela <dggonzalesch@unl.edu.ec>
2022-02-20 20:40:33 -0500

    segundo commit

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+from crypt import methods
 from flask import Flask
-
+from pyspark import F
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return 'MODELO DE PREDICCION CON MACHINE LEARNING'
+
+@app.route("/predecir", methods=["POST"])
+def predecir():
+    return 'Predicciones page'
 
 if __name__ == "__main__":
     app.run()
