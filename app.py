558efbaba4c0005524f7e710f61b70ea382b7344
Gabriela <dggonzalesch@unl.edu.ec>
2022-02-21 02:13:12 -0500

    siguiente commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask
+from flask import Flask , render_template
 #from pyspark import F
 from flask_cors import CORS
 
@@ -10,6 +10,37 @@ CORS(app)
 def home():
     return 'MODELO DE PREDICCION CON MACHINE LEARNING'
 
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    item = [x for x in request.form.values()]
+    data = []
+
+    data.append(int(item[0]))
+    
+    
+    if item[1] == 'Education':
+        data.append(0)
+        data.append(1)
+    else:
+        data.append(1)
+        data.append(0)
+
+    if item[2] == 'Relation':
+        data.append(1)
+        data.append(0)
+    else:
+        data.append(0)
+        data.append(1)
+    
+    prediction = model.predict([data])
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='The Insurance cost will be   $ {}'.format(output))
 
 if __name__ == "__main__":
     app.run()
